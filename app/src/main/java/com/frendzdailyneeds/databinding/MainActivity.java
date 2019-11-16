2a9b8c0725268cdcc2c54bde51508bc79d541014
Sharmin Alam Nipu <you@example.com>
2019-11-16 22:01:30 +0600

    Add setOnclicklistener on textview!!

--- app/src/main/java/com/frendzdailyneeds/databinding/MainActivity.java
+++ app/src/main/java/com/frendzdailyneeds/databinding/MainActivity.java
@@ -3,7 +3,9 @@ package com.frendzdailyneeds.databinding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.frendzdailyneeds.databinding.databinding.ActivityMainBinding;
 
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
        binding.tvOne.setText("This is first Line");
        binding.tvTwo.setText("This is second Line");
-       binding.tvThree.setText("This is third Line");
+       binding.tvThree.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(MainActivity.this,TutorialTwo.class));
+           }
+       });
     }
 }
